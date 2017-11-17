472e86b425eacd6448b9804fac5bc1dcddb9d6b1
notary <nkviz40k@gmail.com>
2017-11-17 21:55:12 +0300

    safe json

--- packages/bem-priv-class/src/decorators/safe.ts
+++ packages/bem-priv-class/src/decorators/safe.ts
@@ -1,19 +1,71 @@
-/* tslint:disable */
+const jsonHook = 'json';
+
+function wrapToTryCatch(method: () => void, callback: ErrorCallback, defaultValue = {}) {
+    return function() {
+        try {
+            return method.apply(this, arguments);
+        } catch (e) {
+            callback(e);
+        }
+
+        return defaultValue;
+    };
+}
+
 export type ErrorCallback = (e: Error) => void;
 
-export function safe<T>(callback: ErrorCallback): Function {
+export function safe(callback: ErrorCallback, defaultValue?: any): MethodDecorator {
     return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
         const method = descriptor.value;
 
-        descriptor.value = function(): T {
+        descriptor.value = function() {
             try {
                 return method.apply(this, arguments);
-            } catch(e) {
+            } catch (e) {
                 callback(e);
-            } finally {
-                return {} as T;
+            }
+
+            if (defaultValue) {
+                return defaultValue;
+            }
+
+            if (propertyKey === jsonHook) {
+                return '';
             }
         };
+    };
+}
+
+/* tslint:disable */
+export function safeBlock(callback: ErrorCallback): Function {
+    return (target: FunctionConstructor): Function => {
+        return class extends target {
+            constructor() {
+                super();
+
+                for (let propName in this) {
+                    const method = this[propName];
+
+                    if (typeof method === 'function') {
+                        this[propName] = wrapToTryCatch(method, callback);
+                    }
+                }
+            }
+        }
+    };
+}
+
+
+export function safeJson(callback: ErrorCallback): Function {
+    return (target: FunctionConstructor): Function => {
+        return class extends target {
+            constructor() {
+                super();
+
+                const method = this[jsonHook];
+                this[jsonHook] = wrapToTryCatch(method, callback, '');
+            }
+        }
     };
 }
 /* tslint:enable */
--- packages/bem-priv-class/test/safe.ts
+++ packages/bem-priv-class/test/safe.ts
@@ -1,23 +1,28 @@
-import { safe } from '../src/decorators/safe';
+import { safe, safeJson, safeBlock } from '../src/decorators/safe';
 import { Block } from '../src/index';
 import { assert } from 'chai';
 
-describe('safe()', () => {
-    let consoleMock: any;
+let consoleMock: any;
+type Throwable = { throw: boolean };
 
+function createConsoleMock() {
+    return {
+        calls: 0,
+        log(e: Event) {
+            this.calls += 1;
+        }
+    };
+}
+
+describe('safe()', () => {
     beforeEach(() => {
-        consoleMock = {
-            calls: 0,
-            log(e: Event) {
-                this.calls += 1;
-            }
-        };
+        consoleMock = createConsoleMock();
     });
 
-    it('LogError', () => {
+    it('should log error', () => {
         const log = (e: Error) => { consoleMock.log(e); };
 
-        class MyComp extends Block<{}> {
+        class MyComp extends Block<Throwable, {}> {
             public get defaultParams() {
                 return {
                     throw: true
@@ -47,6 +52,174 @@ describe('safe()', () => {
 
         const myComp = new MyComp();
         myComp.json();
+
+        assert.strictEqual(consoleMock.calls, 1);
+    });
+});
+
+describe('safeJson()', () => {
+    beforeEach(() => {
+        consoleMock = createConsoleMock();
+    });
+
+    it('should log error', () => {
+        const log = (e: Error) => { consoleMock.log(e); };
+
+        @safeJson(log)
+        class MyComp extends Block<Throwable, {}> {
+            public get defaultParams() {
+                return {
+                    throw: true
+                };
+            }
+
+            public json() {
+                this.mix = [{block: 'test'}, {block: 'test2'}];
+                this.js = {
+                    live: false,
+                    data: {
+                        testData: 50
+                    }
+                };
+                this.mods = {
+                    test: true
+                };
+
+                if (this.params.throw) {
+                    throw new Error('Test Error');
+                }
+
+                return super.json();
+            }
+        }
+
+        const myComp = new MyComp();
+        myComp.json();
+
+        assert.strictEqual(consoleMock.calls, 1);
+    });
+
+    it('should log error if parend was decorate', () => {
+        const log = (e: Error) => { consoleMock.log(e); };
+
+        @safeJson(log)
+        class MyComp extends Block<Throwable, {}> {
+            public get defaultParams() {
+                return {
+                    throw: true
+                };
+            }
+
+            public json() {
+                this.mix = [{block: 'test'}, {block: 'test2'}];
+                this.js = {
+                    live: false,
+                    data: {
+                        testData: 50
+                    }
+                };
+                this.mods = {
+                    test: true
+                };
+
+                return super.json();
+            }
+        }
+
+        class ChildComp extends MyComp {
+            public json() {
+                if (this.params.throw) {
+                    throw new Error('Test Error');
+                }
+
+                return super.json();
+            }
+        }
+
+        const childComp = new ChildComp();
+        childComp.json();
+
+        assert.strictEqual(consoleMock.calls, 1);
+    });
+});
+
+describe('safeBlock()', () => {
+    beforeEach(() => {
+        consoleMock = createConsoleMock();
+    });
+
+    it('should log error', () => {
+        const log = (e: Error) => { consoleMock.log(e); };
+
+        @safeBlock(log)
+        class MyComp extends Block<Throwable, {}> {
+            public get defaultParams() {
+                return {
+                    throw: true
+                };
+            }
+
+            public test() {
+                this.mix = [{block: 'test'}, {block: 'test2'}];
+                this.js = {
+                    live: false,
+                    data: {
+                        testData: 50
+                    }
+                };
+                this.mods = {
+                    test: true
+                };
+
+                if (this.params.throw) {
+                    throw new Error('Test Error');
+                }
+            }
+        }
+
+        const myComp = new MyComp();
+        myComp.test();
+
+        assert.strictEqual(consoleMock.calls, 1);
+    });
+
+    it('should log error if parend was decorate', () => {
+        const log = (e: Error) => { consoleMock.log(e); };
+
+        @safeBlock(log)
+        class MyComp extends Block<Throwable, {}> {
+            public get defaultParams() {
+                return {
+                    throw: true
+                };
+            }
+
+            public test() {
+                this.mix = [{block: 'test'}, {block: 'test2'}];
+                this.js = {
+                    live: false,
+                    data: {
+                        testData: 50
+                    }
+                };
+                this.mods = {
+                    test: true
+                };
+            }
+        }
+
+        class ChildComp extends MyComp {
+            public test() {
+                if (this.params.throw) {
+                    throw new Error('Test Error');
+                }
+
+                return super.test();
+            }
+        }
+
+        const childComp = new ChildComp();
+        childComp.test();
 
         assert.strictEqual(consoleMock.calls, 1);
     });
